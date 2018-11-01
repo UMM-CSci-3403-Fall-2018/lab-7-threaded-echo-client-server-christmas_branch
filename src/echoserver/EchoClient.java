9aa8c593ee8d653f0772d98c59cd62a1fadf9db0
M1NN <score@morris.umn.edu>
2018-11-01 11:23:47 -0500

    Start work on adding threads to echoClient - Added thread for reading from standard input and writing to the server - Added thread for reading from server and writing to standard output - Code currently loops forever, plz fix

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -5,6 +5,40 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
+class KeyboardReader implements Runnable {
+	public static OutputStream socketOutput;
+	public KeyboardReader(OutputStream socketOutput) {
+		this.socketOutput = socketOutput;
+	}
+	public void run() {
+		try{
+			int readByte;
+			while ((readByte = System.in.read()) != -1) {
+				socketOutput.write(readByte);
+				socketOutput.flush();
+			}
+		} catch (IOException io) {}
+		}
+	}
+
+	class ServerReader implements Runnable {
+		public static InputStream socketInput;
+		public ServerReader(InputStream socketInput) {
+			this.socketInput = socketInput;
+		}
+
+		public void run()  {
+			try {
+				int socketByte;
+				while((socketByte = socketInput.read()) != -1) {
+					System.out.write(socketByte);
+				}
+			} catch (IOException io) {}
+			}
+		}
+
+
+
 public class EchoClient {
 	public static final int PORT_NUMBER = 6013;
 
@@ -14,15 +48,30 @@ public class EchoClient {
 	}
 
 	private void start() throws IOException {
-		Socket socket = new Socket("localhost", PORT_NUMBER);
-		InputStream socketInputStream = socket.getInputStream();
-		OutputStream socketOutputStream = socket.getOutputStream();
-		int readByte;
-		while ((readByte = System.in.read()) != -1) {
-			socketOutputStream.write(readByte);
-			int socketByte = socketInputStream.read();
-			System.out.write(socketByte);
+
+		try {
+
+			Socket socket = new Socket("localhost", PORT_NUMBER);
+
+			InputStream socketInputStream = socket.getInputStream();
+			OutputStream socketOutputStream = socket.getOutputStream();
+			int readByte;
+
+			Thread output = new Thread(new KeyboardReader(socketOutputStream));
+			output.start();
+
+			Thread input = new Thread(new ServerReader(socketInputStream));
+			input.start();
+
+			output.join();
+			input.join();
+
+			System.out.flush();
+
+			socket.close();
+		} catch (InterruptedException iex) {
+			System.out.println("Exception in thread: "+iex.getMessage());
 		}
-		System.out.flush();
+
 	}
 }
